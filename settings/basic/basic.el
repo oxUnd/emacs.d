28a04034976c0bd53db19e8c52bea5ac1546b691
xiangshouding <fansekey@gmail.com>
2020-11-04 14:28:18 +0800

    [UPDATE] Use Source Code Pro  font

--- settings/basic/basic.el
+++ settings/basic/basic.el
@@ -5,3 +5,11 @@
 ; backup settings
 (setq backup-directory-alist
              (list (cons "." (concat (getenv "HOME") "/.emacs.d/backups/"))))
+
+;; frame style settings
+(add-to-list 'default-frame-alist '(width . 90))
+(add-to-list 'default-frame-alist '(height . 40))
+(add-to-list 'default-frame-alist '(font . "Source Code Pro-15"))
+
+;; buffer style settings
+
