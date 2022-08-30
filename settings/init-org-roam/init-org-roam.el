bbcf07f9a25ec24530977d7478f9d1bdac4f4755
xiangshouding <fansekey@gmail.com>
2022-08-30 08:58:26 +0800

    [UPDATE] ref direct db

--- settings/init-org-roam/init-org-roam.el
+++ settings/init-org-roam/init-org-roam.el
@@ -1,20 +1,23 @@
 (provide 'init-org-roam)
 
 (get-package 'org-roam)
+
 (get-package 'use-package)
-; (require 'org-roam)
+
 (require 'use-package)
 
 (use-package org-roam
-	     :ensure t
-	     :custom
-	     (org-roam-directory (file-truename "~/Documents/org"))
+             :ensure t
+             :custom
+             (org-roam-directory (file-truename "~/Documents/org"))
+	     (org-roam-db-location (file-truename "~/Documents/org/org-roam.db"))
+	     (org-roam-dailies-directory (file-truename "~/Documents/org/daily"))
 	     :bind (("C-c n l" . org-roam-buffer-toggle)
-		    ("C-c n f" . org-roam-node-find)
-		    ("C-c n g" . org-roam-graph)
-		    ("C-c n i" . org-roam-node-insert)
-		    ("C-c n c" . org-roam-capture)
-		    ("C-c n j" . org-roam-dailies-capture-today))
-	     :config
-	     (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
-	     (require 'org-roam-protocol))
+                    ("C-c n f" . org-roam-node-find)
+                    ("C-c n g" . org-roam-graph)
+                    ("C-c n i" . org-roam-node-insert)
+                    ("C-c n c" . org-roam-capture)
+                    ("C-c n j" . org-roam-dailies-capture-today))
+             :config
+             (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+             (require 'org-roam-protocol))
