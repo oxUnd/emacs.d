9452f1f766dcfb82786cec76c3aaa596a5189fe4
xiangshouding <fansekey@gmail.com>
2022-08-31 10:58:52 +0800

    [UPDATE] add org-roam-ui

--- settings/init-org-roam/init-org-roam.el
+++ settings/init-org-roam/init-org-roam.el
@@ -1,23 +1,45 @@
 (provide 'init-org-roam)
 
 (get-package 'org-roam)
-
+(get-package 'org-roam-ui)
 (get-package 'use-package)
+(get-package 'deft)
 
 (require 'use-package)
 
+(setq org-roam-directory (file-truename "~/Documents/org"))
+
 (use-package org-roam
-             :ensure t
-             :custom
-             (org-roam-directory (file-truename "~/Documents/org"))
-	     (org-roam-db-location (file-truename "~/Documents/org/org-roam.db"))
-	     (org-roam-dailies-directory (file-truename "~/Documents/org/daily"))
-	     :bind (("C-c n l" . org-roam-buffer-toggle)
-                    ("C-c n f" . org-roam-node-find)
-                    ("C-c n g" . org-roam-graph)
-                    ("C-c n i" . org-roam-node-insert)
-                    ("C-c n c" . org-roam-capture)
-                    ("C-c n j" . org-roam-dailies-capture-today))
-             :config
-             (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
-             (require 'org-roam-protocol))
+  :ensure t
+  :custom
+  (org-roam-directory (file-truename "~/Documents/org"))
+  (org-roam-db-location (file-truename "~/Documents/org/org-roam.db"))
+  (org-roam-dailies-directory (file-truename "~/Documents/org/daily"))
+  (org-roam-graph-executable (file-truename "/opt/homebrew/bin/dot"))
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+	 ("C-c n f" . org-roam-node-find)
+	 ("C-c n g" . org-roam-graph)
+	 ("C-c n i" . org-roam-node-insert)
+	 ("C-c n c" . org-roam-capture)
+	 ("C-c n j" . org-roam-dailies-capture-today))
+  :config
+  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+  (require 'org-roam-protocol))
+
+(use-package org-roam-ui
+  :after org-roam
+  :config
+  (setq org-roam-ui-sync-theme t
+	org-roam-ui-follow t
+	org-roam-ui-update-on-save t
+	org-roam-ui-open-on-start t))
+
+(use-package deft
+  :after org
+  :bind
+  ("C-c n d" . deft)
+  :custom
+  (deft-recursive t)
+  (deft-use-filter-string-for-filename t)
+  (deft-default-extension "org")
+  (deft-directory org-roam-directory))
