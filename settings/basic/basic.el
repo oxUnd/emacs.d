c0bd68d6ca498517ef1fcae58d676932b5f0d314
xiangshouding <fansekey@gmail.com>
2022-08-31 16:13:04 +0800

    [FIX] fix fonts, unicode use Cascadia Code PL, han 方正宋刻本秀楷简体

--- settings/basic/basic.el
+++ settings/basic/basic.el
@@ -9,7 +9,16 @@
 ;; frame style settings
 (add-to-list 'default-frame-alist '(width . 90))
 (add-to-list 'default-frame-alist '(height . 40))
-(add-to-list 'default-frame-alist '(font . "Source Code Pro-15"))
+
+;; fix fonts
+(defun fix-fonts()
+  (interactive)
+  (set-face-attribute 'default nil :font (font-spec :family "Cascadia Code PL" :size 14))
+
+  (set-fontset-font t '(#x4e00 . #x9fff) (font-spec :family "方正宋刻本秀楷简体" :size 16) nil 'prepend))
+
+(if (display-graphic-p)
+    (fix-fonts))
 
 ;; buffer style settings
 
